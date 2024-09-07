412e53e2bfeccd18b697bd09dc0f61520efa9c28
Lyrael Rayne <darkangael@gmail.com>
2024-09-07 20:07:19 +1000

    add tt_test keymap for github issue

---
+++ keyboards/keychron/q1v1/ansi/keymaps/tt_test/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Enable caps-lock LED */
+#define CAPS_LOCK_LED_INDEX 45
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 100
