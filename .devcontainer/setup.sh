182dfd3153019aed4b3541b2f619895f15a4bfd5
Lyrael Rayne <darkangael@gmail.com>
2024-09-07 22:29:50 +1000

    set up to use splitkb vial in devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,9 +15,12 @@ git config --global --add safe.directory "$userspacePath"
 git submodule update --init --recursive
 
 [ -d /workspaces/qmk_firmware ] || git clone https://github.com/qmk/qmk_firmware.git /workspaces/qmk_firmware
+[ -d /workspaces/vial_splitkb ] || git clone https://github.com/splitkb/vial-qmk.git /workspaces/vial_splitkb
 git config --global --add safe.directory /workspaces/qmk_firmware
+git config --global --add safe.directory /workspaces/vial_splitkb
 
-qmk config user.qmk_home=/workspaces/qmk_firmware
+# qmk config user.qmk_home=/workspaces/qmk_firmware
+qmk config user.qmk_home=/workspaces/vial_splitkb
 qmk config user.overlay_dir="$userspacePath"
 
 qmk git-submodule
